b16ff18faf835a0a0c08eaf8bcde51734b914f0d
Jason Shao <jasonshao46@gmail.com>
2023-01-02 14:23:50 -0500

    added comments

--- OscpVISA.py
+++ OscpVISA.py
@@ -13,23 +13,24 @@ class SiglentOSCP:
         self.instr.read_termination = '\n'
         self.instr.write_termination = '\n'
         
-    
+    # Identifies device
     def ID(self):
         print('This device is')       
         print(self.instr.query('*IDN?'))
-         
+        
+    # returns the mean voltage measurement from the oscilloscope channel 1
     def return_meanV(self):
         
+        # retrieves str returned by oscilloscope
         og_str = self.instr.query('C1:PARAMETER_VALUE? MEAN') 
+        # characters before the numerical value
         prefix_len = len("C1:PAVA MEAN,")
         
+        # extract numerical value from returned str
         num_str = og_str[prefix_len : len(og_str) - 1]
         
         return float(num_str)
         
-        
-    def test(self):
-        print('This is a test message')
 
         
         
