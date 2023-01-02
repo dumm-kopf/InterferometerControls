a9e4582cd8589afdd71d6854d5e25c5583dd172d
Jason Shao <jasonshao46@gmail.com>
2023-01-02 14:08:57 -0500

    Updated with minor corrections and better comments.

--- Test1.py
+++ Test1.py
@@ -1,15 +1,23 @@
-from pylablib.devices import Thorlabs
+# -*- coding: utf-8 -*-
+"""
+This code is the first attempt at getting a intensity vs. displacement plot
+of a SHG Inteferometer.It is not well written but it works.
+"""
+
 import pyvisa as visa
-import OSCPcomm
+import OscpVISA
 import numpy
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+from pylablib.devices import Thorlabs
 
-
+#%% 
 rm = visa.ResourceManager()
-
 print(rm.list_resources())
 
-OSCP = OSCPcomm.SiglentOSCP(name = 'USB0::0xF4ED::0xEE3A::SDS1ECDQ2R5643::INSTR')
+# Initializes Oscilloscope
+OSCP = OscpVISA.SiglentOSCP(name = 'USB0::0xF4ED::0xEE3A::SDS1ECDQ2R5643::INSTR')
+
 
 OSCP.ID()
 print(OSCP.return_meanV())
@@ -22,23 +30,25 @@ stage = Thorlabs.KinesisMotor('80840262', scale = stage_scale)
 stage.get_status()
 
 
+# Movement Parameters
 
+# disp = 0.000024
+#%%
 
-# Movement
-
-disp = 0.000024
-
-increment = 80
-dx = 200
+dx = 80    #displacement per step/measurement in internal units
+nn = 200   #number of steps/measurement
 
 # Initialize arrays for data
-position_data = numpy.zeros(dx)
-oscp_data = numpy.zeros(dx)
+position_data = numpy.zeros(nn)
+oscp_data = numpy.zeros(nn)
 
+# Moves stage to a initial position
 stage.move_to(-10000, scale = False)
 stage.wait_move()
 
-for i in range(0, dx, 1): 
+#%% Every iteration moves the stage by dx and records position and OSCP.meanV
+for i in tqdm(range(0, nn, 1), desc="Measureing"): 
+
     
     stage.wait_move()
     
@@ -46,14 +56,19 @@ for i in range(0, dx, 1):
     oscp_data[i] = OSCP.return_meanV()
     
     print(str(OSCP.return_meanV()) + " at " + str(stage.get_position(scale = False)))
+    
     # print(position_data[i])
     # print(stage.get_position(scale = False))
      
-    stage.move_by(increment, scale = False)
+    stage.move_by(dx, scale = False)
     
-    
+#%%  Plot the stage.Position vs OSCP.meanV
 plt.plot(position_data, oscp_data)
-    
+
+
+
+
+#%%
 stage.close()
 
 
